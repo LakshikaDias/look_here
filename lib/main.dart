539c0553f6a3985ba5d135824d23211982439077
Lakshika Dias <lakshika9504@gmail.com>
2020-05-21 04:04:06 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(SignIn());
+
+class SignIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Signin Page',
+        ),
+
+      ),
+      body: Column(
+        children: <Widget>[
+          CircleAvatar(
+            backgroundImage: AssetImage('images/user.png'),
+            radius: 50.0,
+          ),
+          Text(''),
+        ],
+      ),
+    );
+  }
+}
